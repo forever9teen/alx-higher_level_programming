697b36abf0c9f4bb8fbc3731227be0b065c775d2
Forever19 <opeloyeruwasiu123@gmail.com>
2022-12-12 16:05:11 -0800

    TASk 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
     if a_dictionary is not None:
         new_dict = dict(a_dictionary)
@@ -7,3 +6,4 @@ def multiply_by_2(a_dictionary):
             new_dict[k] = v * 2
 
             return new_dict
+
